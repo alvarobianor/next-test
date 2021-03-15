de539be22310604881df3415b6d2dd7bfd095a83
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2021-03-15 00:58:19 -0300

    f2456ty

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  // baseURL: "http://localhost:3333",
+  baseURL: "https://fast-anchorage-47524.herokuapp.com/",
 });
 
 export default api;
