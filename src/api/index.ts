20686c189231de5b08f4566c5f89c44ff1d3c4bf
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2021-03-19 00:10:22 -0300

    adding backend

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:3333",
-  baseURL: "https://fast-anchorage-47524.herokuapp.com/",
+  baseURL: "https://alvarotest.herokuapp.com/",
 });
 
 export default api;
